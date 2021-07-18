159f4c597ceb7b35d5e7878fbe0579c4e9fcdf57
pratik karelia <pratik25h.k@gmail.com>
2021-07-18 18:27:09 +0530

    sessions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 const User = require('./model/user')
 const ejs = require('ejs')
-
+const session = require('express-session')
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/authDemo', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
@@ -13,7 +13,7 @@ mongoose.connect('mongodb://localhost:27017/authDemo', {useNewUrlParser: true, u
     console.log("ERROR")
     console.log(err)
 })
-
+app.use(session({secret:'hehe'}))
 app.set('view engine','ejs');
 app.set('views','views')
 app.use(express.urlencoded({extended:true}));
@@ -22,9 +22,6 @@ app.get('/',(req,res)=>{
     res.send('this is the home page')
 })
 
-app.get('/register',(req,res)=>{
-    res.render('register')
-})
 
 app.get('/login',(req,res)=>{
     res.render('login')
@@ -32,15 +29,24 @@ app.get('/login',(req,res)=>{
 
 app.post('/login',async(req,res)=>{
     const {username,password} = req.body;
-    const validUser = await User.findOne({username});
-    const user = await bcrypt.compare(password, validUser.password)
-    if(!user){
-        res.send('TRY AGAIN')
+    const user = await User.findOne({username:username});
+    const validUser = await bcrypt.compare(password, user.password);
+    if(validUser){
+        req.session.user_id = user._id;
+        res.send('WELCOME');
+        
     }
-    res.send("WELCOME")
-    
+    else{
+        res.send("TRY AGAIN")
+    }
 })
 
+app.get('/register',(req,res)=>{
+    res.render('register')
+})
+app.get('/session',(req,res)=>{
+    res.send(req.session.user_id)
+})
 app.post('/register',async(req,res)=>{
     const {username,password} = req.body
     const hash = await bcrypt.hash(password,12)
@@ -48,13 +54,18 @@ app.post('/register',async(req,res)=>{
         username,
         password:hash
     })
+    req.session.user_id = user._id
     await user.save()
     res.redirect('/')
 })
 
 app.get('/secret',(req,res)=>{
-    res.send("YOU CANNOT SEE ME UNTIL U LOGGED IN")
+    if(!req.session.user_id){
+        res.redirect('/')
+    }
+    res.send("YAY U ARE LOGGED IN")
 })
+
 
 app.listen(3000,()=>{
     console.log("Serving")
